b4d86ac2f5a6edab2862f2691d36b0240edbbfd1
ldy <1181661905@qq.com>
2024-09-12 11:40:48 +0800

    升级Ruby，Jekyll，NodeJs，以及Gulp到5.0.0版本

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'), // 压缩js文件
-    sass = require('gulp-sass'), // 编译sass
+    // sass = require('gulp-sass'), // 编译sass
+    sass = require('gulp-sass')(require('sass')),
     cleanCSS = require('gulp-clean-css'), // 压缩css文件
     rename = require('gulp-rename'); // 文件重命名
 
-gulp.task('scripts', function(){
+gulp.task('scripts', function(done){
     gulp.src('dev/js/index.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('assets/js'))
+        .pipe(gulp.dest('assets/js'));
+    done();
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function(done){
     gulp.src('dev/sass/app.scss')
         .pipe(sass())
         .pipe(gulp.dest('dev/sass'))
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('assets/css'));
+    done();
 });
 
 gulp.task('watch', function(){
-    gulp.watch('dev/sass/*.scss', ['sass']);
-    gulp.watch('dev/js/*.js', ['scripts']);
+    gulp.watch('dev/sass/*.scss', gulp.series(['sass']));
+    gulp.watch('dev/js/*.js', gulp.series(['scripts']));
 });
 
-gulp.task('default', ['scripts', 'sass', 'watch']);
+// gulp.task('default', ['scripts', 'sass', 'watch']);
+gulp.task('default', gulp.series('scripts', 'sass', 'watch', function() {}));
